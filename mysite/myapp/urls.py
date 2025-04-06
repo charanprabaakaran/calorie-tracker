39171d8edbade9a09a68e9897051b2b079369dd3
EC2 Default User <ec2-user@ip-172-31-7-24.eu-west-1.compute.internal>
2025-04-06 22:37:35 +0000

    Revert "ui changes"
    This reverts commit eb09fc6bdfdaa3c17268d30f93ead6f1cb94dfe6.

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from . import views  # Import views from the current app
+from myapp import views
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.index, name='index'),  # Home page
     path('register/', views.register, name='register'),  # Register page
     path('accounts/', include('django.contrib.auth.urls')),  # Built-in auth views (login/logout)
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
