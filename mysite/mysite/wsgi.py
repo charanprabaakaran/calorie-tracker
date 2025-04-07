e86a320cdac71f1da6d548ea105868e49fe4fb11
EC2 Default User <ec2-user@ip-172-31-7-24.eu-west-1.compute.internal>
2025-04-07 00:19:43 +0000

    few changes

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
+import sys
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-print("==== WSGI starting up ====")  # Debug print
-print("==== SETTINGS_MODULE:", os.getenv("DJANGO_SETTINGS_MODULE"))
+# Add the parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
