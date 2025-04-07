cf196d41d8eae5d16679ad5f23d5e3c8d2311f6c
EC2 Default User <ec2-user@ip-172-31-7-24.eu-west-1.compute.internal>
2025-04-07 00:47:51 +0000

    wsgi

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -5,6 +5,6 @@ from django.core.wsgi import get_wsgi_application
 # Add the parent directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite/mysite.settings')
 
 application = get_wsgi_application()
